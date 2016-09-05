5bd6ddcd5c1701b1b4481cbee7d43aec3fcbe022
Roman <kaboom.ua@gmail.com>
2016-09-05 23:51:22 +0300

    Update commentIdSource.js

--- src/middlewares/commentIdSource.js
+++ src/middlewares/commentIdSource.js
@@ -3,8 +3,9 @@ import {ADD_COMMENT} from "../constants";
 
 export default store => next => action => {
     console.log('comment middleware', action);
-    
+  //Мидлвары должны быть максимально универсальными. Тебе и в будущем может понадобиться логика генерации id, не привязывайся к типу екшина
     if (action.type === ADD_COMMENT) {
+        //Лучше просто random или guid, тут у тебя не будет гарантий уникальности
         let maxId = Math.max(...store.getState().comments.toJS().map(item => item.id));
         next({...action, commentId: ++maxId});
     } else {
